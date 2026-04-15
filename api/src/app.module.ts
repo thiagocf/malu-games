9da6a7a5b6f8a0013aedb061803f0a9552872e9d
Thiago Fernandes <thiago.fernandes@stay.com.br>
2026-04-15 07:25:49 -0300

    feat(api): add health check endpoint

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './modules/users/users.module';
     PrismaModule,
     AuthModule,
     UsersModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
